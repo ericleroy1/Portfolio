4bc6c58dded68ed584b815415db6c359b4aa1f01
ericleroy1 <54584831+ericleroy1@users.noreply.github.com>
2021-06-11 18:21:49 +1000

    initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p>I am a web developer based in Sydney, Australia.</p>
+            <p>I design beautiful, fast and secure websites.</p>
+        </div>
+    )
+}
+
+export default About
